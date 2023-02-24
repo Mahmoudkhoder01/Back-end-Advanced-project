1cfd7133e0b1497b84f48a4ec83cf22397bbc79f
taghrid <taghridtaleb1@gmail.com>
2023-02-24 09:19:02 +0200

    attendance

---
+++ app/Models/attendance.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class attendance extends Model
+{
+    use HasFactory;
+
+    // protected $fillable = [
+        
+    //     'status',
+    // ];
+    
+
+    // public function section(){
+    //     return $this->belongsTo(section::class);
+    // }
+
+    //  public function student(){
+    //     return $this->belongsTo(student::class);
+    // }
+
+    // public function students(){
+    //     return $this->belongsToMany(student::class);
+    // }
+
+
+    
+}
